9c1bf03ba57317816eeafd9487d6a66f2601b8a9
Mateusz Adamowski <m.adamowski@alx.pl>
2021-04-12 10:55:26 +0200

    zmienne - c.d.

--- skrypt04.sh
+++ skrypt04.sh
@@ -6,4 +6,10 @@ adres="ul. Jasna 14/16a 00-041 Warszawa"
 
 echo "Nazywam się $imie i pracuję w $firma ($adres)"
 
+zwierze=pies
+
+echo "Mały $zwierze to ${zwierze}ek"
+
+# to przypisanie innego imienia nie ma wpływu na polecenia wykonane wcześniej
+imie=Marcin
 
