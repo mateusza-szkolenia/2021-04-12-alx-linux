5a0acaf342a276a8cda78455e371cae44adec7b2
Mateusz Adamowski <m.adamowski@alx.pl>
2021-04-12 15:21:15 +0200

    kopia

---
+++ cw6.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+opcja=J
+rozszerzenie=xz
+
+format_daty="+%Y%m%d %H%M%S"
+dzisiaj="$(date "$format_daty")"
+pakiet=findutils
+archiwum="/tmp/backup-$dzisiaj.tar.$rozszerzenie"
+
+echo "Przechodzę do katalogu /usr/share/doc"
+cd /usr/share/doc
+
+echo "Pakuję pliki"
+tar "${opcja}cvf" "$archiwum" "$pakiet"-*
+
+echo "Utworzono backup findutilsów"
