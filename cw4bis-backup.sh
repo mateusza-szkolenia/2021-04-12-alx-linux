8fceb558a98e3c3663fafca4fc63a383b453785d
Mateusz Adamowski <m.adamowski@alx.pl>
2021-04-12 11:25:30 +0200

    dalsze parametry

--- cw4bis-backup.sh
+++ cw4bis-backup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 dzisiaj=$(date +%Y%m%d-%H%M)
+pakiet=findutils
+archiwum=/tmp/backup-$dzisiaj.tar.gz
 
 echo "Przechodzę do katalogu /usr/share/doc"
 cd /usr/share/doc
 
 echo "Pakuję pliki"
-tar zcvf /tmp/backup-$dzisiaj.tar.gz findutils-*
+tar zcvf $archiwum $pakiet-*
 
 echo "Utworzono backup findutilsów"
