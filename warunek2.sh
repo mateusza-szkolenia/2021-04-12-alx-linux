8f2bdd42ffcb39af3c2c6c180e8b47ccc428dd19
Mateusz Adamowski <m.adamowski@alx.pl>
2021-04-12 16:17:01 +0200

    komentarz

--- warunek2.sh
+++ warunek2.sh
@@ -2,6 +2,8 @@
 
 read -p "Podaj nazwę shella: " shell
 
+# grep -q nic nie wyświetla, a jedynie sygnalizuje kodem wyjścia, czy udało się dopasować wzorzec czy nie
+
 if grep -q "$shell" /etc/passwd
 then
     echo "Mamy użytkowników używających $shell"
