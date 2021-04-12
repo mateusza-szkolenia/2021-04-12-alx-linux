683fb08cc0dd608bd8482de3b2dc679ec5d1f5f1
Mateusz Adamowski <m.adamowski@alx.pl>
2021-04-12 16:06:01 +0200

    parametr + stderr do /dev/null + literówka

--- warunek1.sh
+++ warunek1.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-konto=intruz
+konto="$1"
 
-if id "$konto" > /dev/null
+if id "$konto" > /dev/null 2>&1
 then
    echo "Konto "$konto" istnieje w systemie"
    echo "Wszystko OK"
 else
    echo "Brak konta $konto"
-   echo "Może chcesz jest założyć?"
+   echo "Może chcesz je założyć?"
 fi
 
