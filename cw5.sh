33f6d6a48ff4fba10f9e0588813b593f96929bbd
Mateusz Adamowski <m.adamowski@alx.pl>
2021-04-12 11:49:03 +0200

    zmienne z opcjami kompresji

--- cw5.sh
+++ cw5.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+
+opcja=j
+rozszerzenie=bz2
+
 format_daty="+%Y%m%d %H%M%S"
 dzisiaj="$(date "$format_daty")"
 pakiet=findutils
-archiwum="/tmp/backup-$dzisiaj.tar.gz"
+archiwum="/tmp/backup-$dzisiaj.tar.$rozszerzenie"
 
 echo "Przechodzę do katalogu /usr/share/doc"
 cd /usr/share/doc
 
 echo "Pakuję pliki"
-tar zcvf "$archiwum" "$pakiet"-*
+tar "${opcja}cvf" "$archiwum" "$pakiet"-*
 
 echo "Utworzono backup findutilsów"
