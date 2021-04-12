a797980df5af55a2726e4b48c4c57ddacf801712
Mateusz Adamowski <m.adamowski@alx.pl>
2021-04-12 10:41:06 +0200

    bieżąca data

--- cw4-backup.sh
+++ cw4-backup.sh
@@ -4,6 +4,6 @@ echo "Przechodzę do katalogu /usr/share/doc"
 cd /usr/share/doc
 
 echo "Pakuję pliki"
-tar zcvf /tmp/backup.tar.gz findutils-*
+tar zcvf /tmp/backup-$(date +%Y%m%d).tar.gz findutils-*
 
 echo "Utworzono backup findutilsów"
