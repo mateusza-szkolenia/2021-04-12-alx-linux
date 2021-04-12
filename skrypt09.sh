5f1193191ca5bc3bc3e82091e8d553bf2d71bc51
Mateusz Adamowski <m.adamowski@alx.pl>
2021-04-12 14:32:00 +0200

    zakresy

--- skrypt09.sh
+++ skrypt09.sh
@@ -4,3 +4,9 @@
 
 ./skrypt08.sh raport-{wiosna,lato,jesien,zima}.txt
 
+# liczby od 3 do 7 włącznie
+./skrypt08.sh raport-{3..7}.txt
+
+# liczby od 009 do 013 włącznie (z zerami wiodącymi)
+./skrypt08.sh raport-{009..013}.txt
+
