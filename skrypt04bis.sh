716172f20b6c29af19607624dd87426945ce5bb5
Mateusz Adamowski <m.adamowski@alx.pl>
2021-04-12 14:18:45 +0200

    kopia

---
+++ skrypt04bis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+imie=Mateusz
+firma=ALX
+adres="ul. Jasna 14/16a 00-041 Warszawa"
+
+echo "Nazywam się $imie i pracuję w $firma ($adres)"
+
+zwierze=pies
+
+echo "Mały $zwierze to ${zwierze}ek"
+
+# to przypisanie innego imienia nie ma wpływu na polecenia wykonane wcześniej
+imie=Marcin
+
