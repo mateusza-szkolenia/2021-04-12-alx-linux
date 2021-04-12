2d6c2c1009ab6c4c5c6489e9f2f08f4dede88ec7
Mateusz Adamowski <m.adamowski@alx.pl>
2021-04-12 10:32:03 +0200

    katalog docelowy: /tmp

--- cw3-backup.sh
+++ cw3-backup.sh
@@ -4,6 +4,6 @@ echo "Przechodzę do katalogu /usr/share/doc"
 cd /usr/share/doc
 
 echo "Pakuję pliki"
-tar zcvf backup.tar.gz findutils-*
+tar zcvf /tmp/backup.tar.gz findutils-*
 
 echo "Utworzono backup findutilsów"
