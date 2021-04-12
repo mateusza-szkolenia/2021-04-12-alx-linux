d28351f9a84ebab4d677392c6560802a1d713b10
Mateusz Adamowski <m.adamowski@alx.pl>
2021-04-12 13:26:39 +0200

    wiele zmiennych

---
+++ skrypt06.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Podaj imie i nazwisko:"
+read imie nazwisko
+
+echo "Imie: $imie"
+echo "Nazwisko: $nazwisko"
+
+
+# pierwsze podane słowo wpada do zmiennej imie
+# drugie i kolejne do zmiennej nazwisko
