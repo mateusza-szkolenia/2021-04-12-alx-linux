9f53882446e4d7b5b43bb7618a462ba64a167add
Mateusz Adamowski <m.adamowski@alx.pl>
2021-04-12 16:30:07 +0200

    porównania

---
+++ warunek4.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+read -p "Ile masz lat? " wiek
+
+# porównywanie liczb
+# ge = greater or equal - większy albo równy
+if test "$wiek" -ge 18
+then
+	echo "Jesteś pełnoletni"
+else
+	echo "Jesteś niepełnoletni"
+fi
+
+# inne porównania:
+#  gt - większy         >
+#  lt - mniejszy        <
+#  eq - równy           =
+#  le - mniejszy równy  <=
+#  ge - większy równy   >=
