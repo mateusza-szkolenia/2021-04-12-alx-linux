9090ee01a82d14c9c19d8ae2a8d3c244069f34af
Mateusz Adamowski <m.adamowski@alx.pl>
2021-04-12 10:59:32 +0200

    zmienna z datą bieżącą

--- cw4bis-backup.sh
+++ cw4bis-backup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+dzisiaj=$(date +%Y%m%d-%H%M)
+
 echo "Przechodzę do katalogu /usr/share/doc"
 cd /usr/share/doc
 
 echo "Pakuję pliki"
-tar zcvf /tmp/backup-$(date +%Y%m%d).tar.gz findutils-*
+tar zcvf /tmp/backup-$dzisiaj.tar.gz findutils-*
 
 echo "Utworzono backup findutilsów"
