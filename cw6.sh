807ce7740f3adde56ba72aee7dca71900699320d
Mateusz Adamowski <m.adamowski@alx.pl>
2021-04-12 15:24:09 +0200

    krok 1

--- cw6.sh
+++ cw6.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-
+katalog="$1"
 opcja=J
 rozszerzenie=xz
 
 format_daty="+%Y%m%d %H%M%S"
 dzisiaj="$(date "$format_daty")"
-pakiet=findutils
 archiwum="/tmp/backup-$dzisiaj.tar.$rozszerzenie"
 
-echo "Przechodzę do katalogu /usr/share/doc"
-cd /usr/share/doc
-
 echo "Pakuję pliki"
-tar "${opcja}cvf" "$archiwum" "$pakiet"-*
+tar "${opcja}cvf" "$archiwum" "$katalog"
 
-echo "Utworzono backup findutilsów"
+echo "Utworzono backup $katalog"
+
