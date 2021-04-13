0e47ac8fbbfc74712ccb95dc8107c2fc8244ffc0
Mateusz Adamowski <m.adamowski@alx.pl>
2021-04-13 13:59:36 +0200

    printf - liczby

--- petle7.sh
+++ petle7.sh
@@ -4,7 +4,7 @@ for rok in {2011..2021}
 do
 	for pora in wiosna lato jesien zima
 	do
-		printf "Pora roku: %s      Rok: %s\n" "$pora" "$rok"
+		printf "Pora roku: %-6s    Rok: %-4s\n" "$pora" "$rok"
 	done
 
 done
